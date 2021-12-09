3f6cd837b1e047f5c546dda9087b5cc9c8a695d5
AsherWhi <89489616+AsherWhi@users.noreply.github.com>
2021-12-08 22:31:10 -0800

    Clean up code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 
+
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject winTextObject;
 
+
     private Rigidbody rb;
     private int count;
     private float movementX;
     private float movementY;
-    // Start is called before the first frame update
+    
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -24,6 +27,7 @@ public class PlayerController : MonoBehaviour
         winTextObject.SetActive(false);
     }
 
+
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -31,6 +35,8 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
+
+
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
@@ -40,12 +46,14 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
     private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
     }
+
 
     private void OnTriggerEnter(Collider other)
     {
